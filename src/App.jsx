c855fe60bea3a6e2ca3a1358aa22b89852e43d90
fiiwebarry <fiiwebarry@yahoo.com>
2023-10-17 02:22:03 +0100

    update-on-ecommerce

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import Products from './Components/Product'
+
+function App() {
+
+
+  return (
+    <>
+      <Products />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Product.jsx
@@ -0,0 +1,49 @@
+import axios from "../API/axios";
+
+import { useEffect, useState } from "react";
+
+
+
+
+const Products = () => {
+
+    const [isLoading, setIsLoading] = useState(false)
+    const [products, setProducts] = useState([])
+
+    const getProducts = async () => {
+
+        try {
+            setIsLoading(true);
+            const response = await axios.get("/products")
+            setProducts(response.data)
+        }
+        catch (error) {
+            console.log(error);
+
+        }
+
+
+    };
+
+    useEffect(() => {
+
+        getProducts();
+    }, [])
+    return (<div>
+
+        <div>
+            {products.map((product) => {
+                const { id, title } = product;
+                return (
+                    <div key={id}>
+                        {title}
+
+                    </div>
+                )
+            })}
+            Products</div>
+
+    </div>)
+}
+
+export default Products;
